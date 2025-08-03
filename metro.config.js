bc679447319521793cce15b48f0919e00aee927e
Tkp-7663 <tharakorn.pangporn2547@gnail.com>
2025-08-03 17:49:39 +0700

    Enhance Metro configuration to support additional asset file types

--- metro.config.js
+++ metro.config.js
@@ -4,8 +4,32 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const defaultConfig = getDefaultConfig(__dirname);
+
+const config = {
+    resolver: {
+        assetExts: [
+            ...defaultConfig.resolver.assetExts,
+            "gltf",
+            "glb",
+            "bin",
+            "obj",
+            "mtl",
+            "hdr",
+            "vrx",
+            "arobject",
+            "gif",
+            "PNG",
+            "png",
+            "JPG",
+            "jpg",
+            "jpeg",
+            "usdz",
+        ],
+    },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
